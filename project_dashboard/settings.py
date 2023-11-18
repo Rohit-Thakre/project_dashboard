f0f197d434ba87ceb7512727b781a8e9260162fa
Rohit Thakre <thake785@gmail.com>
2023-11-18 12:41:27 +0530

    for testing using sqlite db.

--- project_dashboard/settings.py
+++ project_dashboard/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'project_dashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-         'NAME':'ecom',
-         'USER': os.environ.get('RDS_USER'),
-         'PASSWORD':os.environ.get('RDS_PASSWORD'),
-         'HOST': os.environ.get('RDS_HOST'),
-         'PORT':'5432'
-     }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#          'NAME':'ecom',
+#          'USER': os.environ.get('RDS_USER'),
+#          'PASSWORD':os.environ.get('RDS_PASSWORD'),
+#          'HOST': os.environ.get('RDS_HOST'),
+#          'PORT':'5432'
+#      }
+# }
 
 
 
