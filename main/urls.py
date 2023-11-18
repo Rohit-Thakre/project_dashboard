645bca959943c20bd90ed50a3b68b012fd092424
Rohit Thakre <thake785@gmail.com>
2023-11-18 11:48:47 +0530

    read json check done.

--- main/urls.py
+++ main/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from main import views
 urlpatterns = [
     path('',views.home, name='home'),
+    path('read_json/', views.read_json, name='read_json')
 ]
--- main/views.py
+++ main/views.py
@@ -7,3 +7,16 @@ from django.http import HttpResponse
 def home(request): 
     
     return HttpResponse('Hello world')
+
+
+from django.views.decorators.csrf import csrf_exempt
+import json
+@csrf_exempt
+def read_json(request): 
+    if request.method == 'POST': 
+        payload = json.loads(request.body)
+        print(payload[0])
+        return HttpResponse('data sent')
+
+    return HttpResponse('Read Json')
+
